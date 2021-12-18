50f89871a7bdb067a70b34d5267333f61e6cb8d7
Antonio Noack <antonio-noack@gmx.de>
2021-12-18 13:05:26 +0100

    solver now works (except boundary conditions)

--- src/me/anno/tsunamis/gpu/compute/TsunamiShaders.kt
+++ src/me/anno/tsunamis/gpu/compute/TsunamiShaders.kt
@@ -1,6 +1,5 @@
 package me.anno.tsunamis.gpu.compute
 
-import me.anno.Engine
 import me.anno.gpu.GFX
 import me.anno.gpu.RenderState.useFrame
 import me.anno.gpu.ShaderLib
@@ -10,25 +9,23 @@ import me.anno.gpu.hidden.HiddenOpenGLContext
 import me.anno.gpu.shader.ComputeShader
 import me.anno.gpu.shader.ComputeShader.Companion.bindTexture
 import me.anno.gpu.shader.ComputeTextureMode
+import me.anno.gpu.shader.builder.Variable
 import me.anno.gpu.texture.Texture2D
+import me.anno.image.ImageWriter
 import me.anno.tsunamis.FluidSim
 import me.anno.tsunamis.setups.CircularDiscontinuity
 import me.anno.utils.OS.desktop
-import me.anno.video.FFMPEGEncodingBalance
-import me.anno.video.FFMPEGEncodingType
-import me.anno.video.VideoCreator
+import me.anno.video.VideoCreator.Companion.renderVideo
 import org.joml.Vector2i
-import org.lwjgl.opengl.ARBShaderImageLoadStore.GL_PIXEL_BUFFER_BARRIER_BIT
-import org.lwjgl.opengl.GL44.glMemoryBarrier
 
-// memory layout: height, momentum x, momentum y, bathymetry
+// texture memory layout: height, momentum x, momentum y, bathymetry
 
-val fWaveSolverBase = "" +
+val fWaveSolverBase = "" + // memory layout: height, momentum, bathymetry
         "   vec2 h  = vec2(data0.x, data1.x);\n" +
         "   vec2 hu = vec2(data0.y, data1.y);\n" +
         "   vec2 b  = vec2(data0.z, data1.z);\n" +
         // dry-wet boundary conditions
-        "   if(h.x <= 0.0){\n" +
+        /*"   if(h.x <= 0.0){\n" +
         "       h.x  = h.y;\n" +
         "       b.x  = b.y;\n" +
         "       hu.x = -hu.y;\n" +
@@ -36,17 +33,21 @@ val fWaveSolverBase = "" +
         "       h.y  = h.x;\n" +
         "       b.y  = b.x;\n" +
         "       hu.y = -hu.x;\n" +
-        "   }\n" +
+        "   }\n" +*/
         "   float roeHeight = (h.x+h.y)*0.5;\n" +
         "   vec2 sqrt01 = sqrt(h);\n" +
-        "   vec2 u = vec2(h.x > 0.0 ? hu.x / h.x : 0.0, h.y > 0.0 ? hu.y / h.y : 0.0);\n" +
+        // "   vec2 u = vec2(h.x > 0.0 ? hu.x / h.x : 0.0, h.y > 0.0 ? hu.y / h.y : 0.0);\n" +
+        "   vec2 u = hu / h;\n" +
         "   float roeVelocity = (u.x * sqrt01.x + u.y * sqrt01.y) / (sqrt01.x + sqrt01.y);\n" +
         "   float gravityTerm = sqrt(gravity * roeHeight);\n" +
-        "   vec2 lambda = roeVelocity + vec2(-gravityTerm, +gravityTerm);\n" +
+        "   vec2 lambda = vec2(roeVelocity - gravityTerm, roeVelocity + gravityTerm);\n" +
         "   float invLambda = 0.5 / gravityTerm;\n" +
-        "   float bathymetryTerm = roeHeight * (b.y - b.x);\n" +
-        "   vec2 df = vec2(hu.y - hu.x, hu.y * u.y - hu.x + gravity * (0.5 * (h.y * h.y - h.x * h.x) + bathymetryTerm));\n" +
-        "   vec2 deltaH  = invLambda * (vec2(df.x * lambda.x, -df.y * lambda.y) - df.y);\n" +
+        "   float bathymetryTerm = gravity * roeHeight * (b.y - b.x);\n" +
+        "   float df0 = hu.y - hu.x;" +
+        "   float df1 = hu.y * u.y - hu.x * u.x" +
+        "                   + gravity * 0.5 * (h.y * h.y - h.x * h.x)" +
+        "                   + bathymetryTerm;\n" +
+        "   vec2 deltaH  = invLambda * vec2(df0 * lambda.y - df1, -df0 * lambda.x + df1);\n" +
         "   vec2 deltaHu = deltaH * lambda;\n"
 
 val fWaveSolverFull = "vec4 solve(vec3 data0, vec3 data1){\n" +
@@ -69,14 +70,28 @@ val fWaveSolverFull = "vec4 solve(vec3 data0, vec3 data1){\n" +
         "   return dst;\n" +
         "}\n"
 
-val fWaveSolverHalf = "vec2 solve(vec3 data0, vec3 data1){\n" +
+val fWaveSolverHalf = "" +
+        "vec2 solveXY(vec3 data0, vec3 data1){\n" +
         fWaveSolverBase +
-        "   vec2 dst;\n" +
+        "   vec2 dst = vec2(0.0);\n" +
         "   if(lambda.x < 0.0){\n" +
-        "       dst.x = deltaH.x;\n" +
-        "       dst.y = deltaHu.x;\n" +
-        "   } else dst = vec2(0.0);\n" +
+        "       dst.x  = deltaH.x;\n" +
+        "       dst.y  = deltaHu.x;\n" +
+        "   }\n" +
         "   if(lambda.y < 0.0){\n" +
+        "       dst.x += deltaH.y;\n" +
+        "       dst.y += deltaHu.y;\n" +
+        "   }\n" +
+        "   return dst;\n" +
+        "}\n" +
+        "vec2 solveZW(vec3 data0, vec3 data1){\n" +
+        fWaveSolverBase +
+        "   vec2 dst = vec2(0.0);\n" +
+        "   if(lambda.x > 0.0){\n" +
+        "       dst.x  = deltaH.x;\n" +
+        "       dst.y  = deltaHu.x;\n" +
+        "   }\n" +
+        "   if(lambda.y > 0.0){\n" +
         "       dst.x += deltaH.y;\n" +
         "       dst.y += deltaHu.y;\n" +
         "   }\n" +
@@ -94,7 +109,7 @@ private fun timeStepShader(x: Boolean): ComputeShader {
                 "uniform vec2 textureSize;\n" +
                 "uniform float timeScale;\n" +
                 "uniform float gravity;\n" +
-                // fWaveSolverFull +
+                fWaveSolverFull +
                 fWaveSolverHalf +
                 "void main(){\n" +
                 "   if(gl_GlobalInvocationID.x < textureSize.x && gl_GlobalInvocationID.y < textureSize.y){\n" +
@@ -105,15 +120,12 @@ private fun timeStepShader(x: Boolean): ComputeShader {
                 "       vec4 data1 = imageLoad(src, uv1);\n" +
                 "       vec4 data2 = imageLoad(src, uv2);\n" +
                 "       vec2 update = timeScale * (${
-                    if (x) "solve(data2.xyw, data1.xyw) + solve(data1.xyw, data0.xyw)"
-                    else "  solve(data2.xzw, data1.xzw) + solve(data1.xzw, data0.xzw)"
+                    if (x) "solveZW(data0.xyw, data1.xyw) + solveXY(data1.xyw, data2.xyw)"
+                    else "  solveZW(data0.xzw, data1.xzw) + solveXY(data1.xzw, data2.xzw)"
                 });\n" +
-                /*"       vec2 update = timeScale * (${
-                    if (x) "solve(data0.xyw, data1.xyw).zw + solve(data1.xyw, data2.xyw).xy"
-                    else "  solve(data0.xzw, data1.xzw).zw + solve(data1.xzw, data2.xzw).xy"
-                });\n" +*/
-                "       imageStore(dst, uv1, vec4(update.x, ${if (x) "update.y, 0.0" else "0.0, update.y"}, 0.0) + data1);\n" +
-                // "       imageStore(dst, uv1, (data0 + data1 + data2)/3.0);\n" +
+                "       vec4 newData = data1 - vec4(update.x, ${if (x) "update.y, 0.0" else "0.0, update.y"}, 0.0);\n" +
+                "       if(newData.x < 0) newData.x = 0;\n" +
+                "       imageStore(dst, uv1, newData);\n" +
                 "   }\n" +
                 "}\n"
     )
@@ -141,39 +153,25 @@ fun computeTimeStep(timeScale: Float, gravity: Float, src: Texture2D, tmp: Textu
     shaderY.runBySize(src.w, src.h)
 }
 
-fun renderVideo(w: Int, h: Int, fps: Double, numIterations: Int, fb: Framebuffer, update: () -> Unit) {
-    val creator = VideoCreator(
-        w, h, fps, numIterations + 1L, FFMPEGEncodingBalance.S1,
-        FFMPEGEncodingType.DEFAULT, desktop.getChild("height.mp4")
-    )
-    creator.init()
-    var frameCount = 0
-    fun writeFrame() {
-        creator.writeFrame(fb, frameCount.toLong()) {
-            frameCount++
-            if (frameCount <= numIterations) {
-                GFX.addGPUTask(1) {
-                    update()
-                    writeFrame()
-                }
-            } else {
-                creator.close()
-                Engine.shutdown()
-            }
-        }
-    }
-    GFX.addGPUTask(1) { writeFrame() }
-    while (!Engine.shutdown) {
-        GFX.workGPUTasks(true)
-    }
-}
+val showWavesShader = ShaderLib.createShader(
+    "copy", ShaderLib.simplestVertexShader, listOf(Variable("vec2", "uv")), "" +
+            "uniform sampler2D tex;\n" +
+            "uniform vec4 mask;\n" +
+            "void main(){\n" +
+            "   float v = dot(texture(tex, uv), mask);\n" +
+            "   gl_FragColor = vec4(vec3(v), 1.0);\n" +
+            "}", listOf("tex")
+)
 
 fun main() {
 
     // test the computation
-    val w = 300
-    val h = 300
+    val w = 50
+    val h = 50
     val numIterations = 500
+
+    val w2 = w * 4
+    val h2 = h * 4
 
     HiddenOpenGLContext.createOpenGL(w, h)
     ShaderLib.init()
@@ -184,7 +182,7 @@ fun main() {
 
     val setup = CircularDiscontinuity()
     setup.heightInner = 0.5f
-    setup.heightOuter = 0.8f
+    setup.heightOuter = 5f
     setup.impulseInner = 0f
     setup.impulseOuter = 0f
     setup.hasBorder = true
@@ -193,10 +191,10 @@ fun main() {
 
     fluidSim.ensureFieldSize()
 
-    val srcFB = Framebuffer("src", w, h, 1, 1, true, DepthBufferType.NONE)
+    val srcFB = Framebuffer("src", w2, h2, 1, 1, true, DepthBufferType.NONE)
     useFrame(srcFB) {}
 
-    val src = srcFB.getColor0()
+    val src = Texture2D("src", w, h, 1)
     val tmp = Texture2D("tmp", w, h, 1)
 
     val fh = fluidSim.fluidHeight
@@ -225,20 +223,29 @@ fun main() {
     src.createRGBA(data, false)
     tmp.createRGBA(data, false)
 
+    ImageWriter.writeImageFloat(w + 2, h + 2, "h.png", false, fh)
+
     val maxTimeStep = fluidSim.computeMaxTimeStep()
     val timeScale = maxTimeStep / fluidSim.cellSizeMeters
 
     // call barrier for memory read
-    glMemoryBarrier(GL_PIXEL_BUFFER_BARRIER_BIT)
+    // glMemoryBarrier(GPIXEBUFFER_BARRIER_BIT)
 
-    // glMemoryBarrier(GL_ALL_BARRIER_BITS)
+    // glMemoryBarrier(GALBARRIER_BITS)
     /*ImageWriter.writeImageFloat(w + 2, h + 2, "h.png", false, fh)
     ImageWriter.writeImageFloat(w + 2, h + 2, "hu.png", false, hu)
     ImageWriter.writeImageFloat(w + 2, h + 2, "hv.png", false, hv)
     ImageWriter.writeImageFloat(w + 2, h + 2, "b.png", false, bh)*/
 
-    renderVideo(w, h, 30.0, numIterations, srcFB) {
+    renderVideo(w2, h2, 30.0, desktop.getChild("height.mp4"), numIterations, srcFB) {
         computeTimeStep(timeScale, fluidSim.gravity, src, tmp)
+        useFrame(srcFB) {
+            src.bind(0)
+            val shader = showWavesShader.value
+            shader.use()
+            shader.v4("mask", 1f, 0f, 0f, 1f)
+            GFX.flat01.draw(shader)
+        }
     }
 
     /* nextFrame()
